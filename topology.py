66e18d755bbc666dd668d83b0cad0383060f2a2c
DhairyaShah981 <76472249+DhairyaShah981@users.noreply.github.com>
2022-11-11 13:29:14 +0530

    Update topology.py

--- topology.py
+++ topology.py
@@ -3,14 +3,14 @@ from mininet.link import TCLink
 from mininet.node import OVSController
 
 
-class MyTopo(Topo, TCLink, OVSController):
+class MyTopo(Topo):
     "Simple topology example."
 
-    def _init_(self):
+    def build(self):
         "Create custom topo."
 
         # Initialize topology
-        Topo._init_(self)
+        Topo.build(self)
 
         # Add hosts and switches
         A = self.addHost('A')
@@ -22,7 +22,7 @@ class MyTopo(Topo, TCLink, OVSController):
 
         # Add links
         L1 = self.addLink(A, R1, cls=TCLink, bw=1000, delay='1ms')
-        L5 = self.addLink(D, R1, cls=TCLink, bw=1000, delay='1ms')
+        L2 = self.addLink(D, R1, cls=TCLink, bw=1000, delay='1ms')
         L3 = self.addLink(B, R2, cls=TCLink, bw=1000, delay='1ms')
         L4 = self.addLink(C, R2, cls=TCLink, bw=1000, delay='5ms')
         L5 = self.addLink(R1, R2, cls=TCLink, bw=500, delay='10ms')
